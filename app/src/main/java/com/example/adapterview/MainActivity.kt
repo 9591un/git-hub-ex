015835abf94be136c2cabefedae1b47e4b67559b
9591un <64430962+9591un@users.noreply.github.com>
2020-06-25 15:14:25 +0900

    주석 달기 수정

--- app/src/main/java/com/example/adapterview/MainActivity.kt
+++ app/src/main/java/com/example/adapterview/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //앱 프로그래밍 시간
+        //Git-hub 업로딩
+        //수정 업로딩
     }
 }
